bd8921150f6b0cc204515cd15086ad0bd1572268
Edu Bernardo <eduluizbernardo@gmail.com>
2024-06-17 22:23:02 -0300

    feat: initial commit

---
+++ ourfinancesapi/src/test/java/com/ourfinances/ourfinancesapi/OurfinancesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ourfinances.ourfinancesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurfinancesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
